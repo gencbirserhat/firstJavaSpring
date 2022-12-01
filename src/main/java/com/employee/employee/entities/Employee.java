e7dc775f963f007d6071c17343bcf803776ebcf7
gencbirserhat <78283289+gencbirserhat@users.noreply.github.com>
2022-12-01 11:02:16 +0300

    Added

---
+++ src/main/java/com/employee/employee/entities/Employee.java
@@ -0,0 +1,31 @@
+package com.employee.employee.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "employees")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue
+    @Column(name = "employee_id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "salary")
+    private double salary;
+}
